948c7b7017dc9f0a03309503c01df7c71461077c
caBatista <caiobatista034@gmail.com>
2025-06-23 23:25:36 -0300

    feat: add Theme

---
+++ src/assets/components/mode-button.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "./theme-provider";
+import { Button } from "./ui/button";
+
+export function ModeButton() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      {theme === "light" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem] transition-all" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem] transition-all" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
